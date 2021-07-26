f18af19e26b47b6f60c06c0408ff7cfe288c8114
Rafael Diniz Soares <rafael.soares@arcomsa.ad>
2021-07-26 16:46:27 -0300

    att

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React, { Component } from "react";
+import Page from './Components/Pages/Page';
+class App extends Component {
+
+
+
+  render() {
+    return (
+      <section className="conteudo">
+        <Page/>
+      </section>
+    );
+  }
+
+}
+
+export default App;
